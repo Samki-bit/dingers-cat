2b667ce45409ec893a6d07093ef9f5882dce4164
samki <ssameep5@gmail.com>
2026-05-09 09:49:55 +0000

    boss

---
+++ scripts/boss/boss.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+ 
+@onready var player = get_parent().find_child("character")
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var progress_bar: ProgressBar = $CanvasLayer/ProgressBar
+
+ 
+var direction : Vector2
+ 
+var health: = 100:
+	set(value):
+		health = value
+		progress_bar.value = value
+		if value <= 0:
+			progress_bar.visible = false
+			find_child("FiniteStateMachine").change_state("Dead")
+ 
+func _ready():
+	set_physics_process(false)
+ 
+ 
+func _process(_delta):
+	direction = player.position - position
+	if direction.x < 0:
+		animated_sprite.flip_h = true
+	else:
+		animated_sprite.flip_h = false
+ 
+func _physics_process(delta):
+	velocity = direction.normalized() * 40
+	move_and_collide(velocity * delta)
+ 
+func take_damage():
+	health -= 5
+ 
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(10)
